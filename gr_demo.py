2e6d8c5fec9c7970e5f7ffc1bc8c83f1ca345a3b
GiacominiTPAI <82030620+GiacominiTPAI@users.noreply.github.com>
2022-02-10 16:42:39 +0100

    Update gr_demo.py

--- gr_demo.py
+++ gr_demo.py
@@ -1,5 +1,5 @@
 from operator import concat
-from turtle import onclick
+#from turtle import onclick
 import streamlit as st
 import pandas as pd
 #from streamlit_player import st_player
@@ -41,7 +41,7 @@ if dis != "Select your disease":
         col1.dataframe(df[col_title])
         col2.write("")
         col2.write("")
-        #col2.button('Edit gene list or choose a different disease', on_click=gr_link)
+        col2.button('Edit gene list or choose a different disease', on_click=gr_link)
         ask=st.button('Ask AI for suggestion')
         bar_placeholder = st.empty()
         if ask:
@@ -59,7 +59,7 @@ if dis != "Select your disease":
                         with c.expander(elem, expanded=False):
                                 col1, col2 = st.columns([1, 1])
                                 col1.write("banana  " + elem)
-                                #col2.button('Inspect Gene Resources', key=elem, on_click=gr_link)
+                                col2.button('Inspect Gene Resources', key=elem, on_click=gr_link)
 
 col1, col2 = st.columns([1, 1])
 col1.text('GeneRecommender Full Version Tutorial')
