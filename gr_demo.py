6f11d3ccf016cd0ebb17456af418554c40cab26d
GiacominiTPAI <82030620+GiacominiTPAI@users.noreply.github.com>
2022-02-11 09:38:55 +0100

    log

--- gr_demo.py
+++ gr_demo.py
@@ -7,6 +7,7 @@ import webbrowser
 import time
 
 def gr_link ():
+        print("go")
         webbrowser.open_new_tab("https://www.generecommender.com/en/dashboard?screen=signup")
 
 data={ 'cancer':
